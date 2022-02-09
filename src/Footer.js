f1fa2aef60c4ab40126fa8ba3e2b318de8288e46
danielsto <daniel.sarmiento@travelperk.com>
2022-02-09 17:36:20 +0100

    Fix progress bar, update socials

--- src/Footer.js
+++ src/Footer.js
@@ -8,13 +8,32 @@ class Footer extends Component {
       <div id="footer-wrapper">
         <p>
           If you enjoyed this just-for-fun project, feel free to say hi on
-          Twitter <a style={{color: "#856300"}} href="https://twitter.com/daniel_swdev">@daniel_swdev</a>{" "}
-          and check out <a style={{color: "#856300"}} href="https://danielsarmiento.dev/">my blog</a>.
+          Twitter{" "}
+          <a
+            style={{ color: "#856300" }}
+            href="https://twitter.com/danieIsarmiento"
+          >
+            @danieIsarmiento
+          </a>{" "}
+          and check out{" "}
+          <a style={{ color: "#856300" }} href="https://danielsarmiento.com/">
+            my blog
+          </a>
+          .
         </p>
         <div className="social-icons">
-          <SocialIcon url="http://twitter.com/daniel_swdev" style={{ margin: "0.5em"}}/>
-          <SocialIcon url="https://www.linkedin.com/in/danielsarmientorocha/" style={{ margin: "0.5em" }}/>
-          <SocialIcon url="https://github.com/danielsto" style={{ margin: "0.5em" }}/>
+          <SocialIcon
+            url="http://twitter.com/danieIsarmiento"
+            style={{ margin: "0.5em" }}
+          />
+          <SocialIcon
+            url="https://www.linkedin.com/in/danielsarmientorocha/"
+            style={{ margin: "0.5em" }}
+          />
+          <SocialIcon
+            url="https://github.com/danielsto"
+            style={{ margin: "0.5em" }}
+          />
         </div>
       </div>
     );
--- src/ProgressMessage.js
+++ src/ProgressMessage.js
@@ -1,47 +1,59 @@
 import React, { Component } from "react";
 import ProgressBar from "react-bootstrap/ProgressBar";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./ProgressMessage.css"
+import "./ProgressMessage.css";
 
 class ProgressMessage extends Component {
-
-  days_passed(dt) {
+  daysPassed(dt) {
     var current = new Date(dt.getTime());
     var previous = new Date(dt.getYear(), 0, 1);
 
     return Math.ceil((current - previous + 1) / 86400000);
   }
 
-  progress_message(progress) {
-      if (progress < 50) {
-          return (
-          <p>We still have a while until the next Bitcoin Pizza Day</p>
-          );
-      } else if (progress >= 50 && progress < 60) {
-          return (
-              <p>We are halfway there until the next Bitcoin Pizza Day!</p>
-          );
-      } else if (progress >= 60 && progress < 80) {
-          return (
-              <p>We are close, Bitcoin Pizza Day is coming!</p>
-          );
-      } else if (progress >= 80) {
-          return (
-              <p>It's almost there! Only a few days until next Bitcoin Pizza Day!!!</p>
-          );
-      }
+  progressMessage(progress) {
+    if (progress < 50) {
+      return <p>We still have a while until the next Bitcoin Pizza Day</p>;
+    } else if (progress >= 50 && progress < 60) {
+      return <p>We are halfway there until the next Bitcoin Pizza Day!</p>;
+    } else if (progress >= 60 && progress < 80) {
+      return <p>We are close, Bitcoin Pizza Day is coming!</p>;
+    } else if (progress >= 80 && progress < 100) {
+      return (
+        <p>
+          It's almost there! Only a few days until next Bitcoin Pizza Day!!!
+        </p>
+      );
+    } else if (progress >= 100) {
+      return (
+        <p>
+          Bitcoin Pizza Day was last May 22. We need to wait until next year
+          now!
+        </p>
+      );
+    }
   }
 
   render() {
-    let daysUntilPizzaDay = this.days_passed(new Date(new Date().getFullYear(), 4, 22));
-    let daysUntilToday = this.days_passed(new Date(2020, 4, 16));
+    const todaysDate = new Date();
+    const todaysDateYear = todaysDate.getFullYear();
+    const startYearDate = new Date(todaysDateYear, 0, 1);
 
-    const progress = Math.floor((daysUntilToday / daysUntilPizzaDay) * 100)
+    const daysUntilPizzaDay = new Date(todaysDateYear, 4, 22) - startYearDate;
+    const daysUntilToday = todaysDate - startYearDate;
+
+    const progress = Math.ceil((daysUntilToday / daysUntilPizzaDay) * 100);
 
     return (
       <div className="progress-wrapper">
-        {this.progress_message(progress)}
-        <ProgressBar style={{backgroundColor: "#FFFFFF"}} animated now={progress} label={`${progress}%`} variant="info"/>
+        {this.progressMessage(progress)}
+        <ProgressBar
+          style={{ backgroundColor: "#FFFFFF" }}
+          animated
+          now={progress}
+          label={`${progress}%`}
+          variant="info"
+        />
       </div>
     );
   }
